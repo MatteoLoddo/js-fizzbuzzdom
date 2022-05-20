2312e830054fdb24c086e9baf60df20c61d7a5f8
MatteoLoddo <loddomatteo96@gmail.com>
2022-05-20 16:32:19 +0200

    add div in  html with js logical

--- js/main.js
+++ js/main.js
@@ -1,4 +1,47 @@
-// CREARE IL  CICLO  DI NUMERI DA 1  A  100
-for (let i= 1;  i<=100;  i++){
-    console.log(i);
-}
+// 
+const containerBoxHtml = document.querySelector(".row")
+
+
+
+
+            // CREARE IL  CICLO  DI NUMERI DA 1  A  100
+    for (let num= 1; num<=100; num++){
+        // let item = num = num
+        // console.log(item);
+
+          // CREATA LA CONDIZIONE CHE SE  IL NUMERO E DIVISIBILE PER 3 STAMPA FIZZ
+        if(((num % 5 == 0) && (num % 3 == 0))){
+            containerBoxHtml.innerHTML += `<div class="col-2">
+                                                <div class="fizz-buzz">
+                                                    ${num}
+                                                </div>
+                                            </div>`
+
+
+
+        // CREATA LA CONDIZIONE CHE SE  IL NUMERO E DIVISIBILE PER 5 STAMPA BUZZ
+        }else if (num % 5 == 0){
+            containerBoxHtml.innerHTML += `<div class="col-2">
+                                                <div class="buzz">
+                                                    ${num}
+                                                </div>
+                                            </div>`
+
+        // CREATA LA CONDIZIONE IN CUI SE ENTRAMBE LE OPERAZIONI SONO VERE STAMPA FIZZBUZZ
+        }else if(num % 3 == 0){
+            containerBoxHtml.innerHTML += ` <div class="col-2">
+                                                <div class="fizz">
+                                                    ${num}
+                                                </div>
+                                            </div>`
+
+
+        }
+        containerBoxHtml.innerHTML += ` <div class="col-2">
+                                                <div class="normal">
+                                                    ${num}
+                                                </div>
+                                            </div>`
+
+    
+    }
